e2e1a00902b588a3b151985fe4a51bfcea88fc99
Rimantas <belauzas@yahoo.com>
2020-04-22 12:37:43 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+import data from './data.js';
+import mustache from './mustache.js';
+
+
+(function(){
+    console.log('done loading!');
+    
+    let password = 'superslaptas';
+
+    function suma(a, b) {
+        const sum = a+b;
+        return sum;
+    }
+
+    console.log(data);
+    
+})();
