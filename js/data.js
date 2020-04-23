6ebc933442b3b31f7d053f49f2b66c9c893c14c4
Rimantas <belauzas@yahoo.com>
2020-04-23 12:38:01 +0300

    Mustache.js

--- js/data.js
+++ js/data.js
@@ -1,3 +1,40 @@
-const benefits = [];
+const mustacheData = {
+    benefits: [
+        {
+            title: 'Silta',
+            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+        },
+        {
+            title: 'Sviesus',
+            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+        },
+        {
+            title: 'Sausas',
+            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+        }
+    ],
+    services: [
+        {
+            title: 'Web design',
+            description: 'The best design you will even not see.',
+            price: 99
+        },
+        {
+            title: 'Web design 2',
+            description: 'The best design you will even see.',
+            price: 87
+        },
+        {
+            title: 'Web design 3',
+            description: 'The best design you will even might see.',
+            price: 20
+        },
+        {
+            title: 'Web design 4',
+            description: 'The best design your GF will even not see.',
+            price: 99999
+        },
+    ]
+};
 
-export default benefits;
+export default mustacheData;
--- js/main.js
+++ js/main.js
@@ -5,15 +5,5 @@ import mustache from './mustache.js';
 
 
 (function(){
-    console.log('done loading!');
-    
-    let password = 'superslaptas';
-
-    function suma(a, b) {
-        const sum = a+b;
-        return sum;
-    }
-
-    console.log(data);
-    
+    mustache(data);
 })();
